c66440682c2acb5ee338a57c7df7a98a564ac117
Roberto <roberto.noya.n@gmail.com>
2018-08-21 02:05:03 +0200

    Step6: named Bake Sets Creation:
    Lists of high and low resolution were added from the step 5.

--- process/step6/step6_ui.py
+++ process/step6/step6_ui.py
@@ -1,8 +1,9 @@
 import bpy
+from ... list.utils import draw_list
 
 #Panel
 class VIEW3D_PT_jet_step6(bpy.types.Panel):
-    bl_label = "Step 6"
+    bl_label = "6. Bake Sets Creation"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_category = "Jet"
@@ -14,7 +15,13 @@ class VIEW3D_PT_jet_step6(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.label("Bake Sets Creation")
+
+        data_path_low = "scene.Jet.list_low_res"
+        draw_list(context, data_path_low, layout, "Low-res", tuple_buttons=(True, False, True, True))
+        if len(context.scene.Jet.list_low_res.obj_list) > 0:
+            idx = context.scene.Jet.list_low_res.obj_list_index
+            data_path_high = "scene.Jet.list_low_res.obj_list[" + str(idx) + "].object.Jet.list_high_res"
+            draw_list(context, data_path_high, layout, "High-res", tuple_buttons=(True, False, True, True))
 
 
 #Operators
